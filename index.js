be666e447a45bcca085e4d3e873b34fbfddee92c
Trakkyx <traefergusson@outlook.com>
2022-03-03 14:57:21 +0000

    commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const productRoute = require("./routes/product");
 dotenv.config();
 
 
-mongoose.connect(process.env.MONGODB_URL ||
+mongoose.connect(
 process.env.MONGO_URL
 ).then(() => console.log("DBconnection Successful"))
 .catch((err) => {
